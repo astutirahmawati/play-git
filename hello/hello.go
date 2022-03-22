3039ee74a98e44b120a369d120baa80617f51ced
astuti <astutirahma13@gmail.com>
2022-03-22 14:13:33 +0700

    initial

---
+++ hello/hello.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// a := "astuti"
+	// b := "rahmawati"
+	fmt.Println("siapa nama kamu?")
+	var pilihan string
+	fmt.Scanln(&pilihan)
+
+	fmt.Printf("hello world %s ", pilihan)
+
+}
---
+++ values/values.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	nama := "astuti"
+	namabelakang := "rahmawati"
+	umur := 23
+	fmt.Printf("Nama Saya: %s %s\numur saya: %v\n", nama, namabelakang, umur)
+	fmt.Println("2+2+4=", 2+2+4)
+	fmt.Println("14/2=", 14/2)
+	fmt.Println(!true)
+	fmt.Println(!false)
+	fmt.Println(true || false)
+	fmt.Println(true && false)
+	fmt.Println("go" + "lang")
+}
